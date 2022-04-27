b500d373863e8ce5793e6b49a41e3b40d02abd5f
ahmedwagehfawzy08 <100706077+ahmedwagehfawzy08@users.noreply.github.com>
2022-04-28 00:13:26 +0200

    Add files via upload

---
+++ SCREEN .py
@@ -0,0 +1,25 @@
+import turtle
+Screen = turtle.screen()
+Screen.srtup(550,600,startx=0,starty=10)
+t=turtle.Turtle()
+turtle.bgcolor('black')
+turtle.color('yellow')
+turtle.speed(11)
+turtle.right(45)
+for i in range(150):
+    turtle.circle(25)
+if 7 < i <62:
+    turtle.left(5)
+if 80 < i <133:
+    turtle.right(5)
+if i <80:
+    turtle.forward(10)
+else :
+    turtle.forward(5)
+
+
+
+
+
+
+
